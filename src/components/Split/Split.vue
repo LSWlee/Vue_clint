a884fcf3bd5f48cb07582a407eb4208452d2ff48
lsw <349986245@qq.com>
2019-01-01 20:02:58 +0800

    完成首屏基本功能

--- src/components/Split/Split.vue
+++ src/components/Split/Split.vue
@@ -1,12 +1,4 @@
-<template>
-  <div class="split"></div>
-</template>
 
-<script type="text/ecmascript-6">
-  export default {
-
-  }
-</script>
 
 <template>
   <div class="split"></div>
--- src/pages/Msite/Msite.vue
+++ src/pages/Msite/Msite.vue
@@ -85,6 +85,7 @@
         })
       }
     },
+
     components:{
       ShopList
     }
